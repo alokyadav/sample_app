9397845c94dfefb544d0475f73936cdd24e24348
alok yadav <yadav.alok29@gmail.com>
2014-05-08 15:26:57 +0530

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -43,7 +43,6 @@ describe "User pages" do
       it { should have_link('delete', href: user_path(User.first)) }
       it "should be able to delete another user" do
         expect do
-          byebug
           click_link('delete', match: :first)
         end.to change(User, :count).by(-1)
       end
