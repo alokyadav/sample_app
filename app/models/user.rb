5021f8625f53427729413707ac6ffe87fbbaf49b
alok yadav <yadav.alok29@gmail.com>
2014-05-09 14:59:26 +0530

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,7 @@ class User < ActiveRecord::Base
 		Digest::SHA1.hexdigest(token.to_s)
 	end
 	def feed
-		# This is preliminary. See "Following users" for the full implementation.
-		Micropost.where("user_id = ?", id)
+		Micropost.from_users_followed_by(self)
 	end
 
 
