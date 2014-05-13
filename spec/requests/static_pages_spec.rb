24d8cc1e24ae3ed60d418beb835ac23460a98389
alok yadav <yadav.alok29@gmail.com>
2014-05-13 16:12:36 +0530

    testing-7

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Tweety') }
+    it { should have_content('RORApp') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
     
